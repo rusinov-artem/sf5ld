8cab273790256f97376f8bcc91f3e6a0d267dc93
RusinovArtem <ar@moderntoken.com>
2020-04-11 17:05:16 +0300

    Symfony5. Annotation routs

--- app/src/Controller/QuestionController.php
+++ app/src/Controller/QuestionController.php
@@ -5,13 +5,26 @@ namespace App\Controller;
 
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGenerator;
 
 class QuestionController
 {
 
+    /**
+     * @Route("/")
+     * @return Response
+     */
     public function homepage()
     {
-        return new Response("Hello symfony 5!");
+        return new Response("Hello symfony 5! ");
+    }
+
+    /**
+     * @Route("/questions/show")
+     */
+    public function show(){
+        return new Response("This is show action");
     }
 
 }
